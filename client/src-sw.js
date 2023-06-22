d893bd90b10f60519e11a3e3ac82f81a3eb7163b
Saritha Koduru <saritha.2708@gmail.com>
2023-06-21 23:52:29 -0700

    implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,30 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implemented asset caching
+registerRoute(
+  ({ request}) => request.destination === 'script' || request.destination === 'style',
+  new CacheFirst({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200], 
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
